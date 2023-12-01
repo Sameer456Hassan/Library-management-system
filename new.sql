0628a56ea63199b64b99f36385c4089712b077b8
Sameer Hassan <sameer.hassan2k2@gmail.com>
2023-12-01 19:57:31 +0500

    feat: final commit

--- new.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO authors (name, dob) VALUES
-    ('J.K. Rowling', '1965-07-31'),
-    ('George Orwell', '1903-06-25'),
-    ('Harper Lee', '1926-04-28'),
-    ('Agatha Christie', '1890-09-15'),
-    ('Stephen King', '1947-09-21');
